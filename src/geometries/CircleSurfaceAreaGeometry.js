c36ffcebacb4ea2aefaac96f4f2f03b383726d37
JediWattson <Robertjd333@yahoo.com>
2017-02-21 15:59:03 -0500

    Update CircleSurfaceAreaGeometry.js

--- src/geometries/CircleSurfaceAreaGeometry.js
+++ src/geometries/CircleSurfaceAreaGeometry.js
@@ -105,7 +105,7 @@ function CircleCircleSurfaceAreaBufferGeometry( radius, segments, thetaStart, th
 
 	for ( i = 1; i <= segments; i ++ ) {
 
-		indices.push( i, i + 1, 0 );
+		indices.push( i, i + 1, z );
 
 	}
 	}
